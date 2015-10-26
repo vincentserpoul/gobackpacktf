7e53f7d8d207d86e7c33857e0658c07cb23324ed
vincentserpoul <vincent@serpoul.com>
2015-10-26 22:01:43 +0800

    adding marketprices API

---
+++ marketprices.go
@@ -0,0 +1,67 @@
+package gobackpacktf
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+// Result is the raw result of the json
+type Result struct {
+	Response `json:"response"`
+}
+
+// Response is the main body of the response
+type Response struct {
+	Success     int                  `json:"success"`
+	Message     string               `json:"message"`
+	CurrentTime int                  `json:"current_time"`
+	Items       map[string]ItemPrice `json:"items"`
+}
+
+// ItemPrice are the items keyed by market hash names. quantity is the qty in the market
+type ItemPrice struct {
+	LastUpdated int `json:"last_updated"`
+	Quantity    int `json:"quantity"`
+	Value       int `json:"value"`
+}
+
+// GetMarketPrices will retrieve prices from the url
+func GetMarketPrices(
+	backpacktfAPIURL string,
+	apiKey string,
+	appID uint32,
+) (*map[string]ItemPrice, error) {
+
+	if backpacktfAPIURL == "" || apiKey == "" || appID == 0 {
+		return nil, fmt.Errorf("gobackpacktf GetMarketPrices no parameter can be empty")
+	}
+
+	querystring := url.Values{}
+	querystring.Add("key", apiKey)
+	querystring.Add("appid", strconv.FormatUint(uint64(appID), 10))
+
+	// regular API string is http://backpack.tf/api/IGetMarketPrices/v1/
+	resp, err := http.Get(backpacktfAPIURL + "?" + querystring.Encode())
+
+	if err != nil {
+		return nil, fmt.Errorf("gobackpacktf GetMarketPrices http.Get: %v", err)
+	}
+
+	defer resp.Body.Close()
+
+	mPrices := Result{}
+
+	err = json.NewDecoder(resp.Body).Decode(&mPrices)
+	if err != nil {
+		return nil, fmt.Errorf("gobackpacktf GetMarketPrices Decode: %v", err)
+	}
+
+	if mPrices.Success != 1 {
+		return nil, fmt.Errorf("gobackpacktf GetMarketPrices not successful: %s", mPrices.Message)
+	}
+
+	return &mPrices.Items, nil
+}
---
+++ marketprices_mock.go
@@ -0,0 +1,59 @@
+package gobackpacktf
+
+// GetMockIGetMarketPrices will return a mock of http://backpack.tf/api/IGetMarketPrices/v1/
+func GetMockIGetMarketPrices() string {
+	return `
+    {
+      "response": {
+        "success": 1,
+        "current_time": 1445863117,
+        "items": {
+          "AK-47 | Aquamarine Revenge (Battle-Scarred)": {
+            "last_updated": 1445860816,
+            "quantity": 85,
+            "value": 1249
+          },
+          "AK-47 | Aquamarine Revenge (Factory New)": {
+            "last_updated": 1445860816,
+            "quantity": 27,
+            "value": 5516
+          },
+          "AK-47 | Cartel (Well-Worn)": {
+            "last_updated": 1445860816,
+            "quantity": 53,
+            "value": 306
+          },
+          "★ StatTrak™ Shadow Daggers | Urban Masked (Well-Worn)": {
+            "last_updated": 1445862648,
+            "quantity": 2,
+            "value": 12690
+          }
+        }
+      }
+    }
+    `
+}
+
+// GetMockKOIGetMarketPrices will return a mock of http://backpack.tf/api/IGetMarketPrices/v1/
+// when the wrong APIkey is used
+func GetMockKOIGetMarketPrices() string {
+	return `
+    {
+      "response": {
+        "success": 0,
+        "message": "This API key is not valid."
+      }
+    }
+    `
+}
+
+// GetMockIGetMarketPrices will return a mock of http://backpack.tf/api/IGetMarketPrices/v1/
+func GetMockBadJSONIGetMarketPrices() string {
+	return `
+      "response": {
+        "success": 0,
+        "message": "This API key is not valid."
+      }
+    }
+    `
+}
